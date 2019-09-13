c0a120900c0a9c15bcb4090edb970ba79897c3c2
shubhamyadav20 <shubhamyadav.sy54@gmail.com>
2019-09-13 01:12:53 -0700

    First Test To Return Positive On Passing Two Positive Integer

---
+++ TddCalApp.Test/CalUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddCalApp.Library;
+
+namespace TddCalApp.Test
+{
+    [TestClass]
+    public class CalUnitTest
+    {
+        [TestMethod]
+        public void ShouldReturnPositiveOnPassingTwoPositiveInteger()
+        {
+            Calculator TestCalculator = new Calculator();
+            int num1 = 4;
+            int num2 = 5;
+            int result;
+            result = TestCalculator.Add(num1, num2);
+            Assert.AreEqual(9, result, "Test Case for Adding 4 and 5");
+        }
+    }
+}
---
+++ TddCalApp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TddCalApp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TddCalApp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc989281-76a9-4503-b742-63eb3592e799")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
